9531e725aeabce39e2d0e116d9a21c2a61136693
OZONE <OZONE@TOT_PLC>
2017-03-07 15:47:00 +0700

    UNO12  ver 182 ***  อย่าลืมวางบน love bot เท่านั้น  เพิ่ม @Component at customer

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Customer.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Customer.java
@@ -12,10 +12,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import org.springframework.stereotype.Component;
 /**
  *
  * @author OZONE
  */
+
+@Component
 @Entity
 public class Customer implements Serializable{
     private static final long serialVersionUID = -3009157732242241606L;
