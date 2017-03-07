b19cca7c458b3b0e3452e9d656bb3be5735d6ae1
OZONE <OZONE@TOT_PLC>
2017-03-07 13:19:06 +0700

    UNO12  ver 182 ***  อย่าลืมวางบน love bot เท่านั้น  เพิ่ม import

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.bot.spring.Customer;
+import com.example.bot.spring.CustomerRepository;
 
 /**
  *
