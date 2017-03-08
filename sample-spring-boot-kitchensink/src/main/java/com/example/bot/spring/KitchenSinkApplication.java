5f01f7c3cf1cf90acca74281889b62bbe806ea94
OZONE <OZONE@TOT_PLC>
2017-03-08 15:06:44 +0700

    UNO15  ver 190 ***  อย่าลืมวางบน love bot ได้แน่ๆ  add entitymanager

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkApplication.java
@@ -19,6 +19,8 @@ package com.example.bot.spring;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +29,8 @@ import org.springframework.context.annotation.Bean;  //eak add
 import org.springframework.cache.annotation.EnableCaching;     // eak add
 import org.springframework.context.annotation.Configuration;    // eak add
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories; //eak add
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration  // eak add
 @EnableJpaRepositories()  //eak add
@@ -34,10 +38,28 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories; //e
 @SpringBootApplication
 public class KitchenSinkApplication {
     static Path downloadedContentDir;
+@Bean
+  public EntityManagerFactory entityManagerFactory() {
 
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setGenerateDdl(true);
+
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setJpaVendorAdapter(vendorAdapter);
+    factory.setPackagesToScan("com.example.bot.spring");
+    factory.setDataSource(dataSource());
+    factory.afterPropertiesSet();
+
+    return factory.getObject();
+  }
+    
     public static void main(String[] args) throws IOException {
         downloadedContentDir = Files.createTempDirectory("line-bot");
         SpringApplication.run(KitchenSinkApplication.class, args);
+    }
+
+    private DataSource dataSource() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
